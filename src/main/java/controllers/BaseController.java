12e4062d66cea644a35fca9c57ea3859f0992ce1
Vyacheclav Bondarenko <wayonmail@gmail.com>
2014-07-25 22:38:08 +0300

    make request to prevent stop gear work on openshift

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -2,10 +2,13 @@ package controllers;
 
 import model.Location;
 
+import javax.ejb.Schedule;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 
@@ -27,5 +30,21 @@ public class BaseController {
         TypedQuery<Location> typedQuery = entityManager.createQuery("SELECT loc FROM Location loc", Location.class);
 
         return typedQuery.getResultList() ;
+    }
+
+
+    @Schedule(hour = "*")
+    public void  makeRequest(){
+        try{
+            String url = "http://petybay.in.ua/api/pet";
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = con.getResponseCode();
+        }
+        catch (Exception e){
+
+        }
     }
 }
